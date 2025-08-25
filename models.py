8b65d3ecb9e9a5efa4427f887255c1ba9bd7cca3
yoshimi-43 <226969011+yoshimi-43@users.noreply.github.com>
2025-08-25 17:17:50 +0900

    models.py修正(認証機能)

--- models.py
+++ models.py
@@ -1,4 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
 # Flask-SQLAlchemyの生成
 db = SQLAlchemy()
@@ -16,3 +18,20 @@ class Memo(db.Model):
   title = db.Column(db.String(50), nullable=False)
   # 内容
   content = db.Column(db.Text)
+
+# ユーザー
+class User(UserMixin, db.Model):
+  # テーブル名
+  __tablename__ = 'users'
+  # ID(PK)
+  id = db.Column(db.Integer, primary_key=True)
+  # ユーザー名
+  username = db.Column(db.String(50), unique=True, nullable=False)
+  # パスワード
+  password = db.Column(db.String(120), nullable=False)
+  # パスワードをハッシュ化して設定する
+  def set_password(self, password):
+    self.password = generate_password_hash(password)
+  # 入力したパスワードとハッシュ化されたパスワードの比較
+  def check_password(self, password):
+    return check_password_hash(self.password, password)
