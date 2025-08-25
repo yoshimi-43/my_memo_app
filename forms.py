f106ec652405d5c3953486627938ab975f6c5ddd
yoshimi-43 <226969011+yoshimi-43@users.noreply.github.com>
2025-08-25 17:30:37 +0900

    forms.py修正(認証処理)

--- forms.py
+++ forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SubmitField
+from wtforms import StringField, TextAreaField, SubmitField, PasswordField
 from wtforms.validators import DataRequired, Length, ValidationError
-from models import Memo
+from models import Memo, User
 
 # =====
 # Formクラス
@@ -24,3 +24,30 @@ class MemoForm(FlaskForm):
     if memo:
       raise ValidationError(f"タイトル '{title.data}' は既に存在します。\
                             別のタイトルを入力してください。")
+
+# ログイン用入力クラス
+class LoginForm(FlaskForm):
+  username = StringField('ユーザー名：', validators=[DataRequired('ユーザー名は必須入力です')])
+  # パスワード：パスワード入力
+  password = PasswordField('パスワード：', validators=[Length(4, 10, 'パスワードの長さは4文字以上10文字以内です')])
+  # ボタン
+  submit = SubmitField('ログイン')
+
+  # カスタムバリデータ
+  # 英文字と記号が含まれているかチェックする
+  def validate_password(self, password):
+    if not (any(c.isalpha() for c in password.data) and \
+      any(c.isdigit() for c in password.data) and \
+      any(c in '!@#$%^&*()' for c in password.data)):
+      raise ValidationError('パスワードには【英数字と記号：!@#$%^&*()】を含める必要があります')
+
+# サインアップ用入力クラス
+class SignUpForm(LoginForm):
+  # ボタン
+  submit = SubmitField('サインアップ')
+
+  # カスタムバリデータ
+  def validate_username(self, username):
+    user = User.query.filter_by(username=username.data).first()
+    if user:
+      raise ValidationError('そのユーザー名は既に使用されています')
