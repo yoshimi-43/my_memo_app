fb79e6ddef8f5c7437162bd0c10ee150e03b3437
yoshimi-43 <226969011+yoshimi-43@users.noreply.github.com>
2025-08-26 11:02:33 +0900

    views.py修正(認証処理)

--- views.py
+++ views.py
@@ -1,11 +1,70 @@
 from flask import render_template, request, redirect, url_for, flash
 from app import app
-from models import db, Memo
-from forms import MemoForm
+from models import db, Memo, User
+from forms import MemoForm, LoginForm, SignUpForm
+from flask_login import login_user, logout_user
 
 # =====
 # ルーティング
 # =====
+# ログイン(Form使用)
+@app.route("/", methods=["GET", "POST"])
+def login():
+  # Formインスタンス生成
+  form = LoginForm()
+  if form.validate_on_submit():
+    # データ入力取得
+    username = form.username.data
+    password = form.password.data
+    # 対象User取得
+    user = User.query.filter_by(username=username).first()
+    # 認証判定
+    if user is not None and user.check_password(password):
+      # 成功
+      # 引数として渡されたuserオブジェクトを使用して、ユーザーをログイン状態にする
+      login_user(user)
+      # 画面遷移
+      return redirect(url_for("index"))
+    # 失敗
+    flash("認証不備です")
+  # GET時
+  # 画面遷移
+  return render_template("login_form.html", form=form)
+
+# ログアウト
+@app.route("/logout")
+def logout():
+  # 現在ログインしているユーザーをログアウトする
+  login_user()
+  # フラッシュメッセージ
+  flash("ログアウトしました")
+  # 画面遷移
+  return redirect(url_for("login"))
+
+# サインアップ
+@app.route("/register", methods=["GET", "POST"])
+def register():
+  # Formインスタンス生成
+  form = SignUpForm()
+  if form.validate_on_submit():
+    # データ入力取得
+    username = form.username.data
+    password = form.password.data
+    # モデルを生成
+    user = User(username=username)
+    # パスワードハッシュ化
+    user.set_password(password)
+    # 登録処理
+    db.session.add(user)
+    db.session.commit()
+    # フラッシュメッセージ
+    flash("ユーザー登録しました")
+    # 画面遷移
+    return redirect(url_for("login"))
+  # GET時
+  # 画面遷移
+  return render_template("register_form.html", form=form)
+
 # 一覧
 @app.route("/memo/")
 def index():
